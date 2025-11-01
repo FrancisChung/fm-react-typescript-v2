af758337a06952019b85902813e513cdfb3bfa6f
Francis Chung <francis.c.chung@gmail.com>
2025-11-01 16:22:25 +1100

    importing reducer

--- accident-counter/src/components/counter.tsx
+++ accident-counter/src/components/counter.tsx
@@ -2,45 +2,76 @@ import {useReducer, useState, Dispatch} from 'react';
 
 const inc = (count: number) => count + 1;
 
-const reducer = (count: number, newValue: number): number => {
-  return newValue;
+type InitialState = {
+  count: number;
+  draftCount: string | number;
 };
 
-type ReducerState = ReturnType<typeof reducer>
+const initialState: InitialState = {
+  count: 0,
+  draftCount: 0,
+};
+
+const reducer = (state = initialState, action: any) => {
+  const { count, draftCount } = state;
+
+  if (action.type === 'increment') {
+    const newCount = count + 1;
+    return { count: newCount, draftCount: newCount };
+  }
+
+  if (action.type === 'decrement') {
+    const newCount = count - 1;
+    return { count: newCount, draftCount: newCount };
+  }
+
+  if (action.type === 'reset') {
+    return { count: 0, draftCount: 0 };
+  }
+
+  if (action.type === 'updateDraftCount') {
+    console.log('updateDraftCount');
+
+    return { count, draftCount: action.payload };
+  }
+
+  if (action.type === 'updateCountFromDraft') {
+    return { count: Number(draftCount), draftCount };
+  }
+
+  return state;
+};
+
 
 const Counter = () => {
     const [count, setCount] = useReducer(reducer, 0);
     const [draftCount, setDraftCount] = useState(count);
 
-const handleClick = () => {
-  setCount(count => count + 1);
-  setCount(count => count + 1);
-  setCount(count => count + 1);
-  }
+    setCount(4);
 
-  return (
-        <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
-          <h1>Days Since the Last Accident</h1>
-          <p className="text-6xl">{count}</p>
-          <div className="flex gap-2">
-            <button onClick={()=>setCount(count =>count-1)}>➖ Decrement</button>
-            <button onClick={() => setCount(count => count =0)}>🔁 Reset</button>
-            <button onClick={()=>setCount(count =>count+1)}>➕ Increment</button>
-          </div>
-          <div>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                setCount(draftCount);
-            }}>
-              <input type="number"
-                     value={draftCount}
-                     onChange={(e) => setDraftCount(e.target.valueAsNumber)}
-              />
-              <button type="submit">Update Counter</button>
-            </form>
-          </div>
-        </section>
-  );
+    return (
+          <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
+            <h1>Days Since the Last Accident</h1>
+            <p className="text-6xl">{count}</p>
+            <div className="flex gap-2">
+              <button onClick={()=>setCount(count =>count-1)}>➖ Decrement</button>
+              <button onClick={() => setCount(count => count =0)}>🔁 Reset</button>
+              <button onClick={()=>setCount(count =>count+1)}>➕ Increment</button>
+            </div>
+            <div>
+              <form onSubmit={(e) => {
+                  e.preventDefault();
+                  setCount(draftCount);
+              }}>
+                <input type="number"
+                       value={draftCount}
+                       onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+                />
+                <button type="submit">Update Counter</button>
+              </form>
+            </div>
+          </section>
+    );
 };
 
 export default Counter;
