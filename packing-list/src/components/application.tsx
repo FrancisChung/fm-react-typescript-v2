0992ae3c88f652064f4df9f894549a7430e81022
Francis Chung <francis.c.chung@gmail.com>
2025-11-24 20:24:09 +1100

    use ItemsContext, useCOntext

--- packing-list/src/components/application.tsx
+++ packing-list/src/components/application.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import {
   createItem,
   filterItems,
@@ -6,34 +6,15 @@ import {
   removeItem,
   updateItem,
 } from '../lib/items';
+
 import Header from './header';
 import ItemList from './item-list';
 import MarkAllAsUnpacked from './mark-all-as-unpacked';
 import NewItem from './new-item';
+import { ItemsContext } from "../context";
 
 const Application = () => {
-  const [items, setItems] = useState(getInitialItems());
-
-  const add = (name: string) => {
-    const item = createItem(name);
-    setItems([...items, item]);
-  };
-
-  const update = (id: string, updates: any) => {
-    setItems(updateItem(items, id, updates));
-  };
-
-  const remove = (id: string) => {
-    setItems(removeItem(items, id));
-  };
-
-  const unpackedItems = filterItems(items, { packed: false });
-  const packedItems = filterItems(items, { packed: true });
-
-  const markAllAsUnpacked = () => {
-    return setItems(items.map((item) => ({ ...item, packed: false })));
-  };
-
+  const {items, add, unpackedItems, update, remove, packedItems, markAllAsUnpacked} = useContext(ItemsContext)
   return (
     <main className="flex flex-col gap-8 p-8 mx-auto lg:max-w-4xl">
       <Header items={items} />
